96fe0322654bdf26a2c7cff96dee4247f5b7d0b5
Adit P Patel <app2116@gmail.com>
2020-01-17 19:55:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -245,8 +245,8 @@ def winning_team
    temp = game_hash()
    home_sum = 0
    away_sum = 0
-   home_points = temp[:home][:players].reduce {|n, home_sum| sum += n[:points]}
-   home_points = temp[:home][:players].reduce {|n, home_sum| sum += n[:points]}
+   home_points = temp[:home][:players].reduce {|n, home_sum| home_sum += n[:points]}
+   home_points = temp[:home][:players].reduce {|n, away_sum| away_sum += n[:points]}
    return temp[:home][:team_name] if home_points > away_points
    return temp[:away][:team_name]
  end
