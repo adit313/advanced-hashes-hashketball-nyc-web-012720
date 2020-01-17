a69daee9e1563ccf37b10a4a56bdf568ad08b67c
Adit P Patel <app2116@gmail.com>
2020-01-17 22:13:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -262,7 +262,7 @@ def player_with_longest_name
     return longest_name
 end
 
-def player_with_longest_name
+def long_name_steals_a_ton?
    temp = game_hash()
    longest_name =  ''
    most_steals = 0
